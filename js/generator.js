d21728b713315003f6101e3c247bd71c4b7c30be
Andrei Lapets <a@lapets.io>
2016-09-03 23:09:54 -0400

    Remove more unused pattern code and replicate pattern on all three pages.

--- js/generator.js
+++ js/generator.js
@@ -1,37 +1,6 @@
 $(function () {
   var patternSize = 240, gridSizeX = 8, gridSizeY = 5;
   var colors = ['#E0533B', '#EBB54A', '#94ED6B', '#73A6FC', '#FFFFFF'];
-  // Set to 0 for canvas.
-  var useSVG = 1;
-
-  /**
-   * Setup listeners for Bootstrap form.
-   */
-  function setupListeners() {
-    $('form :input').change(function (e) {
-      switch (e.target.id) {
-        case 'pattern-size':
-          patternSize = e.target.value;
-          break;
-        case 'grid-size-x':
-          gridSizeX = e.target.value;
-          break;
-        case 'grid-size-y':
-          gridSizeY = e.target.value;
-          break;
-        default:
-          break;
-      }
-
-      // Refresh SVG or Canvas when pattern parameters change
-      if (useSVG) {
-        var svg = setupSVG();
-        drawSVG(svg);
-      } else {
-        drawCanvas();
-      }
-    });
-  }
 
   // -----------------------------------------------------------------------------------
   //
@@ -39,10 +8,9 @@ $(function () {
   //
   // -----------------------------------------------------------------------------------
 
-  function setupSVG() {
-    var svg = d3.select('#svg')
-    // Empty HTML first, parameters might have changed
-      .html('')
+  function setupSVG(id) {
+    var svg = d3.select('#' + id)
+      .html('') // Empty HTML first, parameters might have changed.
       .append('svg')
       .attr('width', patternSize * gridSizeX)
       .attr('height', patternSize * gridSizeY)
@@ -202,89 +170,6 @@ $(function () {
   //
   // -----------------------------------------------------------------------------------
 
-  function drawCanvas() {
-    var canvas = document.getElementById('ctx');
-    if (canvas.getContext) {
-      var ctx = canvas.getContext('2d');
-      var numHor = gridSizeX * 4;
-      var numVer = gridSizeY * 4;
-      var triangleWidth = patternSize / 4;
-
-      var polys = [];
-
-      ctx.canvas.width = patternSize * gridSizeX;
-      ctx.canvas.height = patternSize * gridSizeY;
-
-      for (var j = 0; j < numVer; j++) {
-        for (var i = 0; i < numHor; i++) {
-          var rotated = Math.round(Math.random());
-          var x = i * triangleWidth;
-          var y = j * triangleWidth;
-          var color = randomColor();
-
-
-          drawTriangle(ctx, x, y, triangleWidth, color, false, rotated);
-          polys.push(buildPoly(x, y, triangleWidth, color, false, rotated));
-
-          // Draw second part of square
-          color = randomColor();
-          drawTriangle(ctx, x, y, triangleWidth, color, true, rotated);
-          polys.push(buildPoly(x, y, triangleWidth, color, true, rotated));
-        }
-      }
-    }
-  }
-
-  function drawTriangle(ctx, x, y, width, color, flipped, rotated) {
-    var pi = 3.14159265;
-    var rad;
-
-    ctx.save();
-    // Move origin for rotation
-    ctx.translate(x + width / 2, y + width / 2);
-
-    // Define rotation
-    if (!flipped) {
-      if (rotated) {
-        rad = 270 * pi / 180;
-      } else {
-        rad = 0;
-      }
-    } else {
-      if (rotated) {
-        rad = 90 * pi / 180;
-      } else {
-        rad = pi;
-      }
-    }
-
-    // Rotate and restore canvas position
-    ctx.rotate(rad);
-    ctx.translate(-(x + width / 2), -(y + width / 2));
-
-    // Draw triangle
-    ctx.beginPath();
-    ctx.fillStyle = color;
-    ctx.moveTo(x, y);
-    // Add 1 pixel overlap to first of 2 triangles to fix aliasing issue
-    if (!flipped) {
-      ctx.lineTo(x + width + 1, y);
-      ctx.lineTo(x, y + width + 1);
-    } else {
-      ctx.lineTo(x + width, y);
-      ctx.lineTo(x, y + width);
-    }
-    ctx.fill();
-
-    // Restore canvas
-    ctx.restore();
-
-  }
-
-  function buildPoly(points, color) {
-    return '<polygon fill="' + color + '" points="' + points + '"></polygon>';
-  }
-
   /**
    * Get random color from colors array based on a point in a given integer range.
    * The probability of white being selected is parameterized by the point.
@@ -322,12 +207,11 @@ $(function () {
   // -------------------
   // Application start
   // -------------------
-  setupListeners();
 
-  if (useSVG) {
-    var svg = setupSVG();
-    drawSVG(svg);
-  } else {
-    drawCanvas();
-  }
+  var svg_one = setupSVG('svg_one');
+  var svg_two = setupSVG('svg_two');
+  var svg_three = setupSVG('svg_three');
+  drawSVG(svg_one);
+  drawSVG(svg_two);
+  drawSVG(svg_three);
 });
