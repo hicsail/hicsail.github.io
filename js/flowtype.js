1112220fcae3f7a97d47a6d193ba77093a58a49d
Andrei Lapets <a@lapets.io>
2016-09-08 02:08:15 -0400

    Extend font resizing logic to incorporate height.

--- js/flowtype.js
+++ js/flowtype.js
@@ -28,8 +28,10 @@
       changes = function(el) {
          var $el = $(el),
             elw = $el.width(),
+            elh = $el.height(),
             width = elw > settings.maximum ? settings.maximum : elw < settings.minimum ? settings.minimum : elw,
-            fontBase = width / settings.fontRatio,
+            height = elh > settings.maximum ? settings.maximum : elh < settings.minimum ? settings.minimum : elh,
+            fontBase = Math.min(width / settings.fontRatio, height / settings.fontRatio),
             fontSize = fontBase > settings.maxFont ? settings.maxFont : fontBase < settings.minFont ? settings.minFont : fontBase;
          $el.css('font-size', fontSize + 'px');
       };
