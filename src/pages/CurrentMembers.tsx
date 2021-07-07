e9f1d91fff74a9b2e7d1fed241b377e152e1a509
Christopher Cho <chrischo360@gmail.com>
2021-07-07 19:36:22 -0400

    Current Members page component improved

--- src/pages/CurrentMembers.tsx
+++ src/pages/CurrentMembers.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Heading, Stack, Image, Text } from '@chakra-ui/react';
+import { Box, Flex, Heading, Stack, Image, Text, Link } from '@chakra-ui/react';
 import * as React from 'react';
 import { Layout } from '../components/Layout';
 import { CURRENTMEMBERS_LIST } from '../utils/peopleInformation';
@@ -6,26 +6,49 @@ import { CURRENTMEMBERS_LIST } from '../utils/peopleInformation';
 export const CurrentMembers: React.VFC = () => (
   <Layout title="Current Members">
     {CURRENTMEMBERS_LIST.map(
-      ({ picture, name, role, research, outsideLinks, projects }) => (
-        <Flex direction="row">
+      ({ picture, name, role, outsideLinks, projects }) => (
+        <Flex direction="row" marginBottom="30px">
           <Image
             src={picture}
-            borderRadius="full"
+            borderRadius="100px"
             boxSize="150px"
             objectFit="cover"
           />
           <Box paddingLeft="40px">
             <Flex direction="column">
               <Heading marginBottom="15px">{name}</Heading>
-              <Stack gap={3} fontSize="1.5rem">
+              <Stack gap={3} fontSize="1rem">
                 <Text>
                   <Text as="span" fontWeight="900">
                     Role: {role}
                   </Text>
                 </Text>
-                <Text>Research: {research}</Text>
-                <Text>{outsideLinks}</Text>
-                <Text>{projects}</Text>
+
+                {projects == null ? (
+                  <Text></Text>
+                ) : (
+                  <Flex direction="row">
+                    <Text mr="5px">Projects: </Text>
+                    {projects.map((project) => (
+                      <Flex mr="10px">
+                        <Link href={project.href}>
+                          <Text as="u">{project.name}</Text>
+                        </Link>{' '}
+                      </Flex>
+                    ))}
+                  </Flex>
+                )}
+
+                <Flex direction="row">
+                  <Text mr="5px">Links: </Text>{' '}
+                  {outsideLinks.map((outsideLink) => (
+                    <Flex mr="10px">
+                      <Link href={outsideLink.href}>
+                        <Text as="u"> {outsideLink.name}</Text>
+                      </Link>{' '}
+                    </Flex>
+                  ))}
+                </Flex>
               </Stack>
             </Flex>
           </Box>
