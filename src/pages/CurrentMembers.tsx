4fb455a0b91c66c37fc1002cb876365ff97c33b6
Christopher Cho <chrischo360@gmail.com>
2021-07-08 14:44:25 -0400

    updated image componenet in current members page

--- src/pages/CurrentMembers.tsx
+++ src/pages/CurrentMembers.tsx
@@ -8,75 +8,81 @@ import {
   Link,
   SimpleGrid,
   useColorModeValue,
+  Avatar,
+  useBreakpointValue,
 } from '@chakra-ui/react';
 import * as React from 'react';
 import { Layout } from '../components/Layout';
 import { CURRENTMEMBERS_LIST } from '../utils/peopleInformation';
 
-export const CurrentMembers: React.VFC = () => (
-  <Layout title="Current Members">
-    <SimpleGrid
-      columns={[1, 1, 2]}
-      spacing="40px"
-      pt="3"
-      borderTop={useColorModeValue('2px solid black', '2px solid white')}
-    >
-      {CURRENTMEMBERS_LIST.map(
-        ({ picture, name, role, outsideLinks, projects, bio }) => (
-          <Flex direction="row" marginBottom="30px">
-            <Image
-              src={picture}
-              borderRadius="100px"
-              boxSize={{ base: '100px', md: '150px' }}
-              objectFit="cover"
-            />
-            <Box paddingLeft="40px">
-              <Flex direction="column">
-                <Heading marginBottom="15px">{name}</Heading>
-                <Stack gap={3} fontSize="1rem">
-                  <Text>
-                    <Text as="span" fontWeight="900">
-                      Role: {role}
-                    </Text>
-                  </Text>
+export const CurrentMembers: React.VFC = () => {
+  const avatarSize = useBreakpointValue({ base: 'xl', md: '2xl' });
 
-                  <Text>
-                    <Text as="span" fontWeight="900">
-                      Bio: {bio}
-                    </Text>
-                  </Text>
+  return (
+    <Layout title="Current Members">
+      <SimpleGrid
+        columns={[1, 1, 2]}
+        spacing="40px"
+        pt="3"
+        borderTop={useColorModeValue('2px solid black', '2px solid white')}
+      >
+        {CURRENTMEMBERS_LIST.map(
+          ({ picture, name, role, outsideLinks, projects, bio }) => (
+            <Flex direction="row" marginBottom="30px">
+              <Avatar
+                src={picture}
+                size={avatarSize}
+                fallbackSrc="https://via.placeholder.com/150"
+              />
 
-                  {projects == null ? (
-                    <Text></Text>
-                  ) : (
+              <Box paddingLeft="40px">
+                <Flex direction="column">
+                  <Heading marginBottom="15px">{name}</Heading>
+                  <Stack gap={3} fontSize="1rem">
+                    <Text>
+                      <Text as="span" fontWeight="900">
+                        Role: {role}
+                      </Text>
+                    </Text>
+
+                    <Text>
+                      <Text as="span" fontWeight="900">
+                        Bio: {bio}
+                      </Text>
+                    </Text>
+
+                    {projects == null ? (
+                      <Text></Text>
+                    ) : (
+                      <Flex direction="row">
+                        <Text mr="5px">Projects: </Text>
+                        {projects.map((project) => (
+                          <Flex mr="10px">
+                            <Link href={project.href}>
+                              <Text as="u">{project.name}</Text>
+                            </Link>{' '}
+                          </Flex>
+                        ))}
+                      </Flex>
+                    )}
+
                     <Flex direction="row">
-                      <Text mr="5px">Projects: </Text>
-                      {projects.map((project) => (
+                      <Text mr="5px">Links: </Text>{' '}
+                      {outsideLinks.map((outsideLink) => (
                         <Flex mr="10px">
-                          <Link href={project.href}>
-                            <Text as="u">{project.name}</Text>
+                          <Link href={outsideLink.href}>
+                            <Text as="u"> {outsideLink.name}</Text>
                           </Link>{' '}
                         </Flex>
                       ))}
                     </Flex>
-                  )}
-
-                  <Flex direction="row">
-                    <Text mr="5px">Links: </Text>{' '}
-                    {outsideLinks.map((outsideLink) => (
-                      <Flex mr="10px">
-                        <Link href={outsideLink.href}>
-                          <Text as="u"> {outsideLink.name}</Text>
-                        </Link>{' '}
-                      </Flex>
-                    ))}
-                  </Flex>
-                </Stack>
-              </Flex>
-            </Box>
-          </Flex>
-        ),
-      )}
-    </SimpleGrid>
-  </Layout>
-);
+                  </Stack>
+                </Flex>
+              </Box>
+            </Flex>
+          ),
+        )}
+      </SimpleGrid>
+    </Layout>
+  );
+};
