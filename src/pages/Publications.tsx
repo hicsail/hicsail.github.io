028f004de4cce9b040ebab09a8ce438f99dab4d5
Christopher Cho <chrischo360@gmail.com>
2021-07-08 19:07:15 -0400

    Add content to Publications page

--- src/pages/Publications.tsx
+++ src/pages/Publications.tsx
@@ -1,7 +1,20 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Flex, Stack } from '@chakra-ui/react';
 import * as React from 'react';
 import { Layout } from '../components/Layout';
+import { PUBLICATIONS_LIST } from '../utils/researchInformation';
 
 export const Publications: React.VFC = () => (
-  <Layout title="Publications"></Layout>
+  <Layout title="Publications">
+    <Stack direction={'column'} spacing={4}>
+      {PUBLICATIONS_LIST.map((publication) => (
+        <Flex direction="column">
+          <Box>{publication.title}</Box>
+          <Box>
+            {publication.authors}
+            {publication.date}
+          </Box>
+        </Flex>
+      ))}
+    </Stack>
+  </Layout>
 );
