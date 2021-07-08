c6b602075999e5626366b28de9310099e28312d2
Christopher Cho <chrischo360@gmail.com>
2021-07-08 08:22:32 -0400

    removed ongoing and past research pages

--- src/pages/PastResearch.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-import { PASTRESEARCH_LIST } from '../utils/researchInformation';
-import { ResearchGrid } from '../components/ResearchGrid';
-
-export const PastResearch: React.VFC = () => {
-  return <ResearchGrid title="Past Research" list={PASTRESEARCH_LIST} />;
-};
