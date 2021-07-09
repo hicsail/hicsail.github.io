fba75e7882e08785a4ee08d073847a512db664ee
Christopher Cho <chrischo360@gmail.com>
2021-07-09 16:20:50 -0400

    add alumni and current members pages

--- src/pages/Alumni.tsx
+++ src/pages/Alumni.tsx
@@ -1,4 +1,49 @@
-import { Box } from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  Heading,
+  Stack,
+  Image,
+  Text,
+  Divider,
+} from '@chakra-ui/react';
 import * as React from 'react';
+import { AlumniGrid } from '../components/AlumniGrid';
 
-export const Alumni: React.VFC = () => <Box></Box>;
+export const Alumni: React.VFC = () => (
+  <Flex width="80%" display="column" justifyContent="center">
+    <Box marginBottom="30px">
+      <Heading>Alumni</Heading>
+      <Divider />
+    </Box>
+    <Box>
+      <AlumniGrid />
+      {/* {ALUMNI_LIST.map((person) => (
+          <Flex direction="row" padding="20px 20px 20px 0px">
+            <Image
+              src={person.picture}
+              borderRadius="full"
+              boxSize="150px"
+              objectFit="cover"
+            />
+            <Box paddingLeft="40px">
+              <Flex direction="column">
+                <Heading fontSize="25px">{person.name}</Heading>
+                <Stack spacing={2} fontSize="20x">
+                  <Text>
+                    <Text as="span" fontWeight="900">
+                      Role:
+                    </Text>
+                    {person.role}
+                  </Text>
+                  <Text>Research: {person.research}</Text>
+                  <Text>{person.outsideLinks}</Text>
+                  <Text>{person.projects}</Text>
+                </Stack>
+              </Flex>
+            </Box>
+          </Flex>
+        ))} */}
+    </Box>
+  </Flex>
+);
--- src/pages/CurrentMembers.tsx
+++ src/pages/CurrentMembers.tsx
@@ -1,4 +1,42 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Flex, Heading, Stack, Image, Text } from '@chakra-ui/react';
 import * as React from 'react';
+import { CURRENTMEMBERS_LIST } from '../utils/data';
 
-export const CurrentMembers: React.VFC = () => <Box></Box>;
+export const CurrentMembers: React.VFC = () => (
+  <Flex display="column" justifyContent="center" width="80%">
+    <Box marginBottom="30px">
+      <Heading>Current Members</Heading>
+    </Box>
+    <Box>
+      <Stack direction={'row'} spacing={4}></Stack>
+      {CURRENTMEMBERS_LIST.map((person) => (
+        <Flex direction="row" padding="20px 20px 20px 0px">
+          <Image
+            src={person.picture}
+            borderRadius="full"
+            boxSize="150px"
+            objectFit="cover"
+          />
+          <Box paddingLeft="40px">
+            <Flex direction="column">
+              <Heading fontSize="25px" marginBottom="15px">
+                {person.name}
+              </Heading>
+              <Stack spacing={2} fontSize="20x">
+                <Text>
+                  <Text as="span" fontWeight="900">
+                    Role:
+                  </Text>
+                  {person.role}
+                </Text>
+                <Text>Research: {person.research}</Text>
+                <Text>{person.outsideLinks}</Text>
+                <Text>{person.projects}</Text>
+              </Stack>
+            </Flex>
+          </Box>
+        </Flex>
+      ))}
+    </Box>
+  </Flex>
+);
