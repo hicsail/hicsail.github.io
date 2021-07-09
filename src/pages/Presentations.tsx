7d9bd0c6d70b4e8e70cab439f15850d513d481ac
Christopher Cho <chrischo360@gmail.com>
2021-07-09 16:22:55 -0400

    add presentations page

--- src/pages/Presentations.tsx
+++ src/pages/Presentations.tsx
@@ -1,4 +1,23 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Flex, Heading, Stack, Text } from '@chakra-ui/react';
 import * as React from 'react';
+import { PRESENTATIONS_LIST } from '../utils/data';
 
-export const Presentations: React.VFC = () => <Box></Box>;
+export const Presentations: React.VFC = () => (
+  <Box>
+    <Box>
+      <Heading>Presentations</Heading>
+    </Box>
+    <Box>
+      <Stack direction={'row'} spacing={4}></Stack>
+      {PRESENTATIONS_LIST.map((presentation) => (
+        <Flex direction="column">
+          <Box>{presentation.title}</Box>
+          <Box>
+            {presentation.authors}
+            {presentation.date}
+          </Box>
+        </Flex>
+      ))}
+    </Box>
+  </Box>
+);
