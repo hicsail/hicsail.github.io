8bc1c50a9380370d1916fded51988720a5f1d8b0
Christopher Cho <chrischo360@gmail.com>
2021-07-09 07:52:29 -0400

    Change darkmode background to charcoal color and add an OpportunitiesBanner

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -14,27 +14,29 @@ const theme = extendTheme({
   },
   styles: {
     global: (props) => ({
-      'html, body': {
+      // 'html, body'
+      body: {
         color: mode('black', 'white')(props),
-        bgColor: mode('white', 'black')(props),
+        bgColor: mode('white', '#121212')(props),
         margin: '0 auto',
       },
     }),
   },
+  components: {},
   textStyles: {
     h1: {
       fontWeight: 'black',
       lineHeight: '100px',
     },
     h2: {
-      fontSize: ['1rem', '1.5rem'],
+      fontSize: ['1rem', '2rem'],
       fontWeight: 'bold',
       mt: ['100px'],
     },
     paragraph: {
       display: 'block',
       fontSize: ['1.125rem', '1.25rem'],
-      lineHeight: '1.75',
+      // lineHeight: '1.75',
       marginBlockStart: '1em',
       marginBlockEnd: '1em',
       marginInlineStart: '0px',
@@ -49,3 +51,34 @@ const theme = extendTheme({
 });
 
 export default theme;
+
+// Box: {
+//   baseStyle: (props) => ({
+//     bgColor: mode('white', '#12181b')(props),
+//   }),
+// },
+// Flex: {
+//   baseStyle: (props) => ({
+//     bgColor: mode('white', '#12181b')(props),
+//   }),
+// },
+// Text: {
+//   baseStyle: (props) => ({
+//     bgColor: mode('white', '#12181b')(props),
+//   }),
+// },
+// Grid: {
+//   baseStyle: (props) => ({
+//     bgColor: mode('white', '#12181b')(props),
+//   }),
+// },
+// Heading: {
+//   baseStyle: (props) => ({
+//     bgColor: mode('white', '#12181b')(props),
+//   }),
+// },
+// Link: {
+//   baseStyle: (props) => ({
+//     bgColor: mode('white', '#12181b')(props),
+//   }),
+// },
