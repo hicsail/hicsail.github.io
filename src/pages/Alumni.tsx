e535554ede47817dfc5d0db2b4710334dda16d26
Christopher Cho <chrischo360@gmail.com>
2021-07-13 13:07:19 -0400

    update alumni information page

--- src/pages/Alumni.tsx
+++ src/pages/Alumni.tsx
@@ -1,49 +1,11 @@
-import {
-  Box,
-  Flex,
-  Heading,
-  Stack,
-  Image,
-  Text,
-  Divider,
-} from '@chakra-ui/react';
 import * as React from 'react';
 import { AlumniGrid } from '../components/AlumniGrid';
+import { Layout } from '../components/Layout';
 
-export const Alumni: React.VFC = () => (
-  <Flex width="80%" display="column" justifyContent="center">
-    <Box marginBottom="30px">
-      <Heading>Alumni</Heading>
-      <Divider />
-    </Box>
-    <Box>
+export const Alumni: React.VFC = () => {
+  return (
+    <Layout title="Alumni">
       <AlumniGrid />
-      {/* {ALUMNI_LIST.map((person) => (
-          <Flex direction="row" padding="20px 20px 20px 0px">
-            <Image
-              src={person.picture}
-              borderRadius="full"
-              boxSize="150px"
-              objectFit="cover"
-            />
-            <Box paddingLeft="40px">
-              <Flex direction="column">
-                <Heading fontSize="25px">{person.name}</Heading>
-                <Stack spacing={2} fontSize="20x">
-                  <Text>
-                    <Text as="span" fontWeight="900">
-                      Role:
-                    </Text>
-                    {person.role}
-                  </Text>
-                  <Text>Research: {person.research}</Text>
-                  <Text>{person.outsideLinks}</Text>
-                  <Text>{person.projects}</Text>
-                </Stack>
-              </Flex>
-            </Box>
-          </Flex>
-        ))} */}
-    </Box>
-  </Flex>
-);
+    </Layout>
+  );
+};
