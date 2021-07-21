dfab3aeb2a1ea703bbb596922af2eb32bf9bd892
Christopher Cho <chrischo360@gmail.com>
2021-07-21 10:52:11 -0400

    remove blurb at top of research page

--- src/pages/Research.tsx
+++ src/pages/Research.tsx
@@ -10,12 +10,12 @@ export const Research = () => {
   return (
     <React.Fragment>
       <Box height="5rem" width="50%" m="0 auto">
-        <Text textStyle="paragraph">
+        {/* <Text textStyle="paragraph">
           Since ideation in fall of 2014, SAIL has undertaken over 80 distinct
           software engineering projects (funded by agencies such as NSF, NIH,
           DARPA, and others). Featured here is a non-exhaustive list of current
           and past collaborations.
-        </Text>
+        </Text> */}
       </Box>
       <ResearchGrid title="Ongoing Research" list={ONGOINGRESEARCH_LIST} />
       <ResearchGrid title="Past Research" list={PASTRESEARCH_LIST} />
