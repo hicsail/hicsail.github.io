0244a30bf1688eee46df1aa00c656222016e15f6
Kamila <ukamila@bu.edu>
2023-09-12 16:41:39 -0400

    new photos

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box, Flex } from '@chakra-ui/react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Publications } from './pages/Publications';
 import { Projects } from './pages/Projects';
@@ -7,25 +8,60 @@ import { Alumni } from './pages/Alumni';
 import { People } from './pages/People';
 import { Presentations } from './pages/Presentations';
 import Footer from './components/Footer';
+import { Banner } from './components/Banner';
+import NavigationBar from './components/NavigationBar';
+import { OpportunitiesBanner } from './components/OpportunitiesBanner';
 import { Contact } from './pages/Contact';
 import { Engineer } from './pages/Engineer';
 import { Intern } from './pages/Intern';
 
 export const App: React.FC = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/people" element={<People />} />
-        <Route path="/alumni" element={<Alumni />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/presentations" element={<Presentations />} />
-        <Route path="/publications" element={<Publications />} />
-        <Route path="/technicalstaff" element={<Engineer />} />
-        <Route path="/interns" element={<Intern />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
-  );
+    return (
+        <Box width="100%" height="100%">
+            <Flex
+                direction="column"
+                align="center"
+                margin="0 auto"
+                width="100%"
+                height="100%"
+                minHeight="100vh"
+                justifyContent="flex-start"
+            >
+                <Banner /> {/* Banner component */}
+                <NavigationBar /> {/* NavigationBar component */}
+                <OpportunitiesBanner opportunityAvailable={false} />{' '}
+                {/* OpportunitiesBanner component */}
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Route path="/people">
+                        <People />
+                    </Route>
+                    <Route path="/alumni">
+                        <Alumni />
+                    </Route>
+                    <Route path="/projects">
+                        <Projects />
+                    </Route>
+                    <Route path="/presentations">
+                        <Presentations />
+                    </Route>
+                    <Route path="/publications">
+                        <Publications />
+                    </Route>
+                    <Route path="/technicalstaff">
+                        <Engineer />
+                    </Route>
+                    <Route path="/interns">
+                        <Intern />
+                    </Route>
+                    <Route path="/contact">
+                        <Contact />
+                    </Route>
+                </Switch>
+                <Footer /> {/* Footer component */}
+            </Flex>
+        </Box>
+    );
 };
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,139 +1,143 @@
 import {
-  Box,
-  Button,
-  Modal,
-  ModalBody,
-  ModalCloseButton,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-  ModalOverlay,
-  useDisclosure,
-  Image,
-  Text,
-  useColorModeValue,
-  Link,
-  List,
+    Box,
+    Button,
+    Modal,
+    ModalBody,
+    ModalCloseButton,
+    ModalContent,
+    ModalFooter,
+    ModalOverlay,
+    useDisclosure,
+    Image,
+    Text,
+    useColorModeValue,
+    Link,
+    List,
 } from '@chakra-ui/react';
 import * as React from 'react';
 import { OutsideLink } from '../types/types';
 
 interface Props {
-  modalButtonText: string | undefined;
-  modalHeader: string | undefined;
-  modalBody: string | undefined;
-  imageHref: string | undefined;
-  modalButtonSubText: string | undefined;
-  pi: React.ReactElement | null;
-  metaDataPresentation?: Array<OutsideLink>;
-  metaDataPublication?: Array<OutsideLink>;
+    modalButtonText: string | undefined;
+    modalHeader: string | undefined;
+    modalBody: string | undefined;
+    imageHref: string | undefined;
+    modalButtonSubText: string | undefined;
+    pi: React.ReactElement | null;
+    metaDataPresentation?: Array<OutsideLink>;
+    metaDataPublication?: Array<OutsideLink>;
 }
 
 export const Card: React.FC<Props> = ({
-  modalButtonText,
-  modalHeader,
-  modalBody,
-  imageHref,
-  modalButtonSubText,
-  pi,
-  metaDataPresentation,
-  metaDataPublication,
+    modalButtonText,
+    modalBody,
+    imageHref,
+    modalButtonSubText,
+    pi,
+    metaDataPresentation,
+    metaDataPublication,
 }) => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  return (
-    <>
-      <Box width="100%" onClick={onOpen}>
-        <Box borderBottom="2px solid black">
-          <Image
-            maxWidth="100%"
-            height="10rem"
-            width="100%"
-            objectFit="cover"
-            src={imageHref}
-            fallbackSrc="../../img/research/placeholder-research.jpg"
-          />
-        </Box>
-        <Box p="1.5em" display="block" height="10rem">
-          <Text
-            fontSize="1.75rem"
-            fontWeight="800"
-            fontStyle="normal"
-            color={useColorModeValue('black', 'white')}
-            pb="1rem"
-          >
-            {modalButtonText}
-          </Text>
-          <Text
-            fontSize="1.125rem"
-            color={useColorModeValue('black', '#b2becd')}
-          >
-            {modalButtonSubText}
-          </Text>
-        </Box>
-      </Box>
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    return (
+        <>
+            <Box width="100%" onClick={onOpen}>
+                <Box borderBottom="2px solid black">
+                    <Image
+                        maxWidth="100%"
+                        height="10rem"
+                        width="100%"
+                        objectFit="cover"
+                        src={imageHref}
+                        fallbackSrc="../../img/research/placeholder-research.jpg"
+                    />
+                </Box>
+                <Box p="1.5em" display="block" height="10rem">
+                    <Text
+                        fontSize="1.75rem"
+                        fontWeight="800"
+                        fontStyle="normal"
+                        color={useColorModeValue('black', 'white')}
+                        pb="1rem"
+                    >
+                        {modalButtonText}
+                    </Text>
+                    <Text
+                        fontSize="1.125rem"
+                        color={useColorModeValue('black', '#b2becd')}
+                    >
+                        {modalButtonSubText}
+                    </Text>
+                </Box>
+            </Box>
 
-      <Modal isOpen={isOpen} onClose={onClose} size="2xl">
-        <ModalOverlay />
-        <ModalContent padding="1rem" bg={useColorModeValue('white', '#2a2e35')}>
-          <ModalCloseButton />
-          <ModalBody>
-            <Image
-              // maxWidth="100%"
-              // height="10rem"
-              // width="100%"
-              objectFit="cover"
-              src={imageHref}
-              fallbackSrc="../../img/research/placeholder-research.jpg"
-            />{' '}
-            <Text textStyle="paragraph" m="0">
-              {modalBody}
-            </Text>
-            {metaDataPresentation ?? (
-              <Text textStyle="paragraph" m="0">
-                Presentations
-              </Text>
-            )}
-            {metaDataPresentation?.map(({ name, href }) => (
-              <Text textStyle="paragraph" m="0" color="teal">
-                <Link as="a" href={href}>
-                  {name}
-                </Link>
-              </Text>
-            ))}
-            {metaDataPublication ?? (
-              <Text textStyle="paragraph" m="0">
-                Publications
-              </Text>
-            )}
-            <List>
-              {metaDataPublication?.map(({ name, href }) => (
-                <Text textStyle="paragraph" m="0" color="green">
-                  <Link as="a" href={href}>
-                    {name}
-                  </Link>
-                </Text>
-              ))}
-            </List>
-            {pi == null ? (
-              <Text textStyle="paragraph">{pi}</Text>
-            ) : (
-              <Text textStyle="paragraph">PI: {pi}</Text>
-            )}
-          </ModalBody>
-
-          <ModalFooter>
-            <Button
-              variant="ghost"
-              mr={3}
-              onClick={onClose}
-              textAlign="center"
-              width="100%"
-            >
-              Okay
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-    </>
-  );
+            <Modal isOpen={isOpen} onClose={onClose} size="2xl">
+                <ModalOverlay />
+                <ModalContent
+                    padding="1rem"
+                    bg={useColorModeValue('white', '#2a2e35')}
+                >
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <>
+                            {' '}
+                            <Image
+                                objectFit="cover"
+                                src={imageHref}
+                                fallbackSrc="../../img/research/placeholder-research.jpg"
+                            />{' '}
+                            <Text textStyle="paragraph" m="0">
+                                {modalBody}
+                            </Text>
+                            {metaDataPresentation ?? (
+                                <Text textStyle="paragraph" m="0">
+                                    Presentations
+                                </Text>
+                            )}
+                            {metaDataPresentation?.map(({ name, href }) => (
+                                <Text textStyle="paragraph" m="0" color="teal">
+                                    <Link as="a" href={href}>
+                                        {name}
+                                    </Link>
+                                </Text>
+                            ))}
+                            {metaDataPublication ?? (
+                                <Text textStyle="paragraph" m="0">
+                                    Publications
+                                </Text>
+                            )}
+                            <List>
+                                {metaDataPublication?.map(({ name, href }) => (
+                                    <Text
+                                        textStyle="paragraph"
+                                        m="0"
+                                        color="green"
+                                    >
+                                        <Link as="a" href={href}>
+                                            {name}
+                                        </Link>
+                                    </Text>
+                                ))}
+                            </List>
+                            {pi == null ? (
+                                <Text textStyle="paragraph">{pi}</Text>
+                            ) : (
+                                <Text textStyle="paragraph">PI: {pi}</Text>
+                            )}
+                        </>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button
+                            variant="ghost"
+                            mr={3}
+                            onClick={onClose}
+                            textAlign="center"
+                            width="100%"
+                        >
+                            Okay
+                        </Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </>
+    );
 };
