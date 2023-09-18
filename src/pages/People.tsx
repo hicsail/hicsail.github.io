f21ffb17de98ecd88e3900bc7e29f146e8de17ee
Kamila <ukamila@bu.edu>
2023-09-18 13:28:57 -0400

    locomotive scroll

--- src/pages/People.tsx
+++ src/pages/People.tsx
@@ -13,6 +13,7 @@ import {
 import * as React from 'react';
 import { Layout } from '../components/Layout';
 import data from '../utils/data/data.json';
+import LocomotiveScroll from 'locomotive-scroll';
 
 interface Person {
   picture: string;
@@ -24,14 +25,21 @@ interface Person {
 
 export const People: React.FC = () => {
   let people: Person[] = data['people'];
+
+  const scroller = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]') as HTMLElement,
+    smooth: true
+  })
+
   return (
-    <Layout title="Current Members">
-      <SimpleGrid columns={[2, 2, 2, 4]} pt="3">
+    <Layout title="Current Members" data-scroll-container>
+      <Text sx={{fontSize:"40px", opacity: 0, transition: '4s'}} data-scroll data-scroll-direction="horizontal" data-scroll-speed='-3'>Welcome to our team</Text>
+      <SimpleGrid columns={[2, 2, 2, 4]} pt="3" columnGap='16px' rowGap='50px' data-scroll-section>
         {people.map(({ picture, name, role, email }, i) => (
           <Flex direction="column" marginBottom="30px" key={i}>
             <Popover trigger="hover">
               <PopoverTrigger>
-                <Box>
+                <Box data-scroll data-scroll-offset="20%">
                   <img src={picture} />
                   <Text
                     fontWeight="900"
