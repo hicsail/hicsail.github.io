4ce863525d18a7bfa064e242a25e725560f09690
ukamila <ukamila@bu.edu>
2023-10-30 16:39:02 -0400

    projects data visual

--- src/components/DataVisual/DataVisual.tsx
+++ src/components/DataVisual/DataVisual.tsx
@@ -14,46 +14,38 @@ import './DataVisual.css';
 
 export const DataVisual: React.FC = () => {
   return (
-    <Card boxShadow={0}>
-      <CardBody backgroundColor="gray.800">
-        <HStack
-          id="stackityStack"
-          margin="5px 20px 5px 20px"
-          justifyContent="space-between"
-        >
-          <Box id="innerCard">
-            <Icon id="cardIcon" as={BiBuilding} color="blue.400" />
-            <Heading id="cardHeading">30+</Heading>
-            <Text id="cardText">
-              Since 2022 we've proudly forged dynamic and enduring partnerships
-            </Text>
-          </Box>
-          <Box id="innerCard">
-            <Icon id="cardIcon" as={BiDollarCircle} color="green.400" />
-            <Heading id="cardHeading">$3M</Heading>
-            <Text id="cardText">Since 2022 we've garnered $3 million in</Text>
-          </Box>
-          <Box id="innerCard">
-            <Icon id="cardIcon" as={BiGroup} color="red.400" />
-            <Heading id="cardHeading">100+</Heading>
-            <Text id="cardText">
-              Collaborated with over 100 visionary faculty members, nurturing
-            </Text>
-          </Box>
-
-          <Box id="innerCard">
-            <Box marginLeft="auto" marginRight="auto">
-              <Pie colour="lightyellow" percentage={83} />
+    <Box>
+      <Card boxShadow={0}>
+        <CardBody backgroundColor="gray.800">
+          <HStack
+            id="stackityStack"
+            margin="0.3em 1.5em 0.3em 1.5em"
+            justifyContent="space-between"
+          >
+            <Box id="innerCard">
+              <Icon id="cardIcon" as={BiBuilding} color="blue.400" />
+              <Heading id="cardHeading">30+</Heading>
+              <Text id="cardText">
+                Dynamic and enduring partnerships we've forged since 2022.
+              </Text>
             </Box>
-            <Heading id="cardHeading" fontSize="17px !important">
-              Percents
-            </Heading>
-            <Text id="cardText">
-              This statistic can be shown through percentages.
-            </Text>
-          </Box>
-        </HStack>
-      </CardBody>
-    </Card>
+            <Box id="innerCard">
+              <Icon id="cardIcon" as={BiDollarCircle} color="green.400" />
+              <Heading id="cardHeading">$3M</Heading>
+              <Text id="cardText">
+                The funding we have received since 2022.
+              </Text>
+            </Box>
+            <Box id="innerCard">
+              <Icon id="cardIcon" as={BiGroup} color="red.400" />
+              <Heading id="cardHeading">100+</Heading>
+              <Text id="cardText">
+                Faculty members we have had pleasure collaborating.
+              </Text>
+            </Box>
+          </HStack>
+        </CardBody>
+      </Card>
+    </Box>
   );
 };
