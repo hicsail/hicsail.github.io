19cca178d5e2df28c7f33e9c4a5ee030e66f027c
Greg Frasco <frascog17@gmail.com>
2023-11-15 14:58:44 -0500

    fix: styling

--- src/components/AboutIcons/AboutIcons.tsx
+++ src/components/AboutIcons/AboutIcons.tsx
@@ -2,14 +2,14 @@ import {
   Box,
   Card,
   CardBody,
+  Heading,
   HStack,
   Icon,
-  Heading,
   useColorModeValue,
 } from '@chakra-ui/react';
 import {
-  BiLaptop,
   BiCodeCurly,
+  BiLaptop,
   BiPencil,
   BiPieChart,
   BiTrophy,
@@ -29,23 +29,23 @@ export const AboutIcons: React.FC = () => {
         <CardBody backgroundColor={useColorModeValue('white', 'gray.800')}>
           <HStack id="stack">
             <Box id="innerCard">
-              <Icon id="cardIcon" as={BiLaptop} color="#178123" />
+              <Icon id="cardIcon" as={BiLaptop} color="#178123" mb="4" />
               <Heading id="h2">{iconText[0]}</Heading>
             </Box>
             <Box id="innerCard">
-              <Icon id="cardIcon" as={BiCodeCurly} color="#1b678d" />
+              <Icon id="cardIcon" as={BiCodeCurly} color="#1b678d" mb="4" />
               <Heading id="h2">{iconText[1]}</Heading>
             </Box>
             <Box id="innerCard">
-              <Icon id="cardIcon" as={BiPieChart} color="#DFDF54" />
+              <Icon id="cardIcon" as={BiPieChart} color="#DFDF54" mb="4" />
               <Heading id="h2">{iconText[2]}</Heading>
             </Box>
             <Box id="innerCard">
-              <Icon id="cardIcon" as={BiTrophy} color="#f58711" />
+              <Icon id="cardIcon" as={BiTrophy} color="#f58711" mb="4" />
               <Heading id="h2">{iconText[3]}</Heading>
             </Box>
             <Box id="innerCard">
-              <Icon id="cardIcon" as={BiPencil} color="#df5454" />
+              <Icon id="cardIcon" as={BiPencil} color="#df5454" mb="4" />
               <Heading id="h2">{iconText[4]}</Heading>
             </Box>
           </HStack>
